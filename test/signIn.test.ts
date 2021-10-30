1d1d7a65b6533bf21feb9a480cdcb03830a5a147
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-10-30 20:54:16 +0530

    inner elements

--- test/signIn.test.ts
+++ test/signIn.test.ts
@@ -4,7 +4,7 @@ describe("handle sign up", () =>{
     let browser: Browser
     let context:BrowserContext
     let page:Page;
-    jest.setTimeout(20000);
+    jest.setTimeout(40000);
     beforeAll(async() =>{
         browser = await chromium.launch({
             headless: false
@@ -16,12 +16,19 @@ describe("handle sign up", () =>{
     test("sign in", async() =>{
         await page.click("a[id='login2']")
         await page.fill("input[id='loginusername']", "jayakrishnaarasu34")
-        await page.fill("input[id='loginpassword']","")
+        await page.fill("input[id='loginpassword']","kicha23031998")
         await page.click("//button[text()='Log in']")
         const userName = await page.$("#nameofuser")
         if(userName!=null){
-            await userName?.click();
-            console.log(userName);
-        }else console.log("null");
+            await userName?.innerText();
+            console.log(userName.$);}
         })
+
+    test("inner elements", async() =>{
+        await page.$("#itemc");    //*laptop button//
+        await page.click("text='Sony vaio i5'");
+        await page.click("a[href='index.html']");
+        await page.$("#itemc");      //*phone button//
+        await page.click("text='Nokia lumia 1520'");
+    })
     })
