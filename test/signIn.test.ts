b6f30e82f3855705bf0a784e57c05d100a859a4b
jayakrishna2398 <jayakrishnakrish1998@gmail.com>
2021-10-30 19:11:37 +0530

    updated

--- test/signIn.test.ts
+++ test/signIn.test.ts
@@ -4,7 +4,7 @@ describe("handle sign up", () =>{
     let browser: Browser
     let context:BrowserContext
     let page:Page;
-    jest.setTimeout(40000);
+    jest.setTimeout(20000);
     beforeAll(async() =>{
         browser = await chromium.launch({
             headless: false
@@ -18,11 +18,10 @@ describe("handle sign up", () =>{
         await page.fill("input[id='loginusername']", "jayakrishnaarasu34")
         await page.fill("input[id='loginpassword']","kicha23031998")
         await page.click("//button[text()='Log in']")
-        // const userName = await page.$("#nameofuser")
-        // if(userName!=null){
-        //     await page?.innerText("id=nameofuser")
-        //     console.log(userName);
-        // }
-        await page.getAttribute("a[id='nameofuser']", "value")
+        const userName = await page.$("#nameofuser")
+        if(userName!=null){
+            await userName?.click();
+            console.log(userName);
+        }else console.log("null");
         })
     })
